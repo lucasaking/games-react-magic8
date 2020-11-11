a41de49805d533a82bfbcc3df671d23eebb02900
lucasaking <70596715+lucasaking@users.noreply.github.com>
2020-11-10 20:28:34 -0500

    fixed ui

--- src/index.js
+++ src/index.js
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import EightBall from './Eightball.js'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App { ...EightBall }/>
   </React.StrictMode>,
   document.getElementById('root')
 );
